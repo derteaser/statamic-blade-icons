3daa64c85deabb20c6fcc10a15ff3f2041293f36
Jens Küsters <jens@kuesters.net>
2026-02-09 12:36:06 +0100

    Remove IconPicker fieldtype and frontend assets
    Delete the IconPicker fieldtype and all related front-end code and build artifacts. Removed src/Fieldtypes/IconPicker.php, resources/js/fieldtypes/IconPicker.vue, resources/js/blade-icon-picker.js, and the generated dist assets/manifest. Updated composer.json to stop advertising a fieldtype and removed the IconPicker registration from ServiceProvider; the Icon tag remains registered.

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,12 +2,10 @@
 
 namespace Derteaser\StatamicBladeIcons;
 
-use Derteaser\StatamicBladeIcons\Fieldtypes\IconPicker;
 use Derteaser\StatamicBladeIcons\Tags\Icon;
 use Statamic\Providers\AddonServiceProvider;
 
 class ServiceProvider extends AddonServiceProvider {
-    protected $fieldtypes = [IconPicker::class];
     protected $tags = [Icon::class];
     protected $vite = [
         'input' => ['resources/js/blade-icon-picker.js'],
