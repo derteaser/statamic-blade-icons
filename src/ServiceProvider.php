4e58ca2c9cfd557af887a211d7cd4e353911cec3
Jens Küsters <jens@kuesters.net>
2026-02-09 12:46:31 +0100

    Remove Vite config from ServiceProvider
    Remove the protected $vite array from ServiceProvider.php, deleting the input, hotFile, and publicDirectory settings. This removes the Vite build configuration previously defined on the addon service provider.

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -7,9 +7,4 @@ use Statamic\Providers\AddonServiceProvider;
 
 class ServiceProvider extends AddonServiceProvider {
     protected $tags = [Icon::class];
-    protected $vite = [
-        'input' => ['resources/js/blade-icon-picker.js'],
-        'hotFile' => 'dist/vite.hot',
-        'publicDirectory' => 'dist',
-    ];
 }
